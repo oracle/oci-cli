f986f75f8b04eb8f3b8e6ad1a97f35f290e6bf32
Hamada Ibrahim <18110511+HamadaGabrIbrahim@users.noreply.github.com>
2021-11-10 12:39:31 -0800

    Update install.ps1

--- scripts/install/install.ps1
+++ scripts/install/install.ps1
@@ -67,8 +67,8 @@ if ([System.Enum]::GetNames('System.Net.SecurityProtocolType') -Contains 'Tls12'
 }
 
 
-$PythonInstallScriptUrl = "https://raw.githubusercontent.com/oracle/oci-cli/v3.2.1/scripts/install/install.py"
-$FallbackPythonInstallScriptUrl = "https://raw.githubusercontent.com/oracle/oci-cli/v2.22.0/scripts/install/install.py"
+$PythonInstallScriptUrl = "https://raw.githubusercontent.com/oracle/oci-cli/v3.3.0/scripts/install/install.py"
+$FallbackPythonInstallScriptUrl = "https://raw.githubusercontent.com/oracle/oci-cli/v3.2.1/scripts/install/install.py"
 $PythonVersionToInstall = "3.8.5"    # version of Python to install if none exists
 $MinValidPython3Version = "3.6.0"    # minimum required version of Python 3 on system
 
