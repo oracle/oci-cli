08178d1ebdfef05e28b5299cf491428222de8102
OCI DEX Release Bot Automation <oci-dex-releases-bot_ww@oracle.com>
2024-06-14 21:17:18 +0000

    Releasing version 3.42.11
    Co-authored-by: Harsh Kumar <h.harsh.kumar@oracle.com>

--- services/apm_config/tests/util/generated/command_to_api.py
+++ services/apm_config/tests/util/generated/command_to_api.py
@@ -8,6 +8,7 @@ SDK_client_map = {
     "apm_config.list_configs": "oci.apm_config.ConfigClient.list_configs",
     "apm_config.retrieve_namespace_metrics": "oci.apm_config.ConfigClient.retrieve_namespace_metrics",
     "apm_config.retrieve_namespaces": "oci.apm_config.ConfigClient.retrieve_namespaces",
+    "apm_config.test": "oci.apm_config.ConfigClient.test",
     "apm_config.update_config": "oci.apm_config.ConfigClient.update_config",
     "apm_config.validate_span_filter_pattern": "oci.apm_config.ConfigClient.validate_span_filter_pattern",
 }
